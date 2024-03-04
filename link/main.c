b3efd0588de8d723a9aeff4b3b7ce1ee2243896c
donghol <162074271+donghol@users.noreply.github.com>
2024-03-03 18:11:36 -0800

    Initialcode

--- link/main.c
+++ link/main.c
@@ -1,1 +1,40 @@
-// Placeholder for link code
+#include <kipr/wombat.h>
+ void front(int ms) {
+       motor(0,100);
+       motor(1,100);
+       msleep(ms);
+   }
+int main()
+{
+     wait_for_light(0);
+    motor(0,100);
+    msleep(170);
+    ao();
+    
+    front(5000);
+    ao();
+    
+    motor(1,100);
+    msleep(1860);
+    ao();
+    
+    front(1900);
+    ao();
+    
+    motor(0,100);
+    msleep(1800);
+    ao();
+    
+    front(2600);
+    ao();
+    
+    motor(0,100);
+    msleep(1500);
+    ao();
+    
+    front(3500);
+    ao();
+    motor(0,-100);
+    motor(1,-115);
+    msleep(4500);
+}
