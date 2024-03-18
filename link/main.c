135f0b7d871c3ff442c8056a493072c8cdda7483
johnlee <johnlee8778@gmail.com>
2024-03-17 18:00:21 -0700

    Link - Helping revert back to latest working

--- link/main.c
+++ link/main.c
@@ -1,46 +1,98 @@
 #include <kipr/wombat.h>
- void front(int ms) {
-       //motor(0,100);
-      // motor(1,100);
-     mav(0,2000);
-     mav(1,2000);
-       msleep(ms);
-   }
-int main()
-{
+//Prototypes
+void move_forward(int ms);
+void move_backwards(int ms);
+void turn_right(int ms);
+void turn_left(int ms);
+void mav_move_forward(int ms);
+void mav_move_backwards(int ms);
+void mav_turn_right(int ms);
+void mav_turn_left(int ms);
+
+//Main
+int main() {
+    //starting
+    //wait_for_light(0);
+    turn_left(170);
+
+    //start 1st straight
+    move_forward(4500);
+   
+    //turn right
+    turn_right(1560);
+
+    //drive on bump
+    move_forward(1700);
     
-   // motor(0,100);
-    mav(0,2000);
-    msleep(170);
-    ao();
+    //turn left off of bump
+    turn_left(1800);
     
-    front(5000);
-    ao();
+    //towards 2nd to last rock
+    move_forward(2500);
     
-    //motor(1,100);
-    mav(1,2000);
-    msleep(1860);
-    ao();
+    //turn left towards the rock heap
+    turn_left(1600);
     
-    front(1900);
+    //drive into the rock heap
+    move_forward(3500);
+
+    //move out of icreate's path
+    move_backwards(3800);
+
+}
+
+//~~~~ FUNCTIONS ~~~~//
+
+//move forward
+void move_forward(int ms) {
+    motor(0, 100);
+    motor(1, 100);
+    msleep(ms);
     ao();
-    
-    //motor(0,100);
-    mav(0,2000);
-    msleep(1800);
+}
+
+void mav_move_forward(int ms) {
+    //mav(0, )
+    //mav(1, )
+    msleep(ms);
     ao();
-    
-    front(2600);
+}
+
+//move backwards
+void move_backwards(int ms) {
+    motor(0,-100);
+    motor(1,-100);
+    msleep(ms);
     ao();
-    
-    //motor(0,100);
-    mav(0,2000);
-    msleep(1500);
+}
+
+void mav_move_backwards(int ms) {
+    //mav(0, )
+    ///mav(1, )
+    msleep(ms);
     ao();
-    
-    front(3500);
+}
+
+//turn right
+void turn_right(int ms) {
+    motor(1, 100);
+    msleep(ms);
     ao();
-    //motor(0,-100);
-    //motor(1,-115);
-    //msleep(4500);
+}
+
+void mav_turn_right(int ms) {
+    //mav(1, );
+    msleep(ms);
+    ao();    
+}
+
+//turn left  
+void turn_left(int ms) {
+    motor(0, 100);
+    msleep(ms);    
+}
+
+void mav_turn_left(int ms) {
+    //mav(0, );
+    msleep(ms);
 }
