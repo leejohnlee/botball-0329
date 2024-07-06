e315a10245b3257b295c906a59cbc6a4cb10b9f0
casper1051 <90978578+casper1051@users.noreply.github.com>
2024-07-05 19:13:03 -0700

    Improve Reliability, clean up code
    Added some movements to the first multiplier grabbing, so now even if the claw catches on the ledge for the second cubes, it should still grab at least one cube.  Also formatted and cleaned up the code

--- GCER Create/main.c
+++ GCER Create/main.c
@@ -25,11 +25,11 @@ void claw_grab_habitat(); // Adjust claw servo to grab habitat elements
 void set_arm_position(int target_pos, float speed); // Set the arm servo to a specific position at a specified speed
 
 // Function declarations for main code chunks
-void get_multipliers(); // Execute actions related to fetching multipliers
+void pom_pyramid_multipliers(); // Execute actions related to fetching multipliers to place in the area with the red and green poms
 void initialize(); // Perform initial setup and connection to the robot
-void pom_area(); // Execute actions related to handling poms in a designated area
+void pom_pyramid_area(); // Execute actions related to handling poms in a designated area
 void purple_tubes(); // Execute actions related to handling purple tubes
-
+void orange_pom_area(); //Execute actions realted to handling multiplier cubes in the orange pom area
 // Global variables
 int left_servo_port = 3; // Left servo motor port
 int right_servo_port = 2; // Right servo motor port
@@ -39,7 +39,7 @@ double velocity = 0.15; // Velocity parameter for specific operations (e.g., bum
 
 int rangefinder_port = 5; // Port for the rangefinder sensor
 
-int claw_open_degrees = 1045; // Degrees to open the claw
+int claw_open_degrees = 900; // Degrees to open the claw, Original value 1045, V2 900
 int claw_closed_degrees = 260; // Degrees to close the claw
 int claw_start_degrees = 1680; // Degrees to set the claw at starting position
 int claw_grab_degrees = 0; // Degrees to grab habitat cubes
@@ -52,11 +52,12 @@ int arm_start_degrees = 550; // Degrees to set the arm at starting position
 // Main function
 int main() {
     initialize(); // Initialize the robot and perform setup
-    while(!side_button()){msleep(1);}
+    while(!side_button()){msleep(1);} //Simulate wait_for_light(), delete when testing complete
     // Execute sections of the total code
-    get_multipliers(); // Fetch multipliers
-    //pom_area(); // Execute actions related to poms
-    //purple_tubes(); // Handle purple tubes
+    pom_pyramid_area(); //Execute actions related to the red and green pom area
+    pom_pyramid_multipliers(); //Execute actions related to placing multipliers in that area
+    orange_pom_area(); //Execute actions related to the orange pom area, such as placing multipliers and pushing orange poms
+    purple_tubes(); //Execute actions related to handling the purple tubes
     return 0;
 }
 
@@ -268,18 +269,11 @@ void initialize() {
     //shut_down_in(119);
     //claw_close();
 }
-void get_multipliers() { //[NOTE] Changed this so in the delay when the Link crosses the center bump, 
-    //the Create3 pushes the poms into a different area, but still can get points form that
-    // Fetch multipliers
-    msleep(250);
-
-    // Slow down to avoid bump offset and close claw to avoid purple tube
-    
+void pom_pyramid_area(){
     claw_close();
     align_to_line();
     arm_up();
-    //[BEGIN_CHANGES]
-    forward(5, 30); // Original value 38
+    forward(5, 30);
     rotate(60);
     forward(4,10);
     arm_down();
@@ -292,95 +286,80 @@ void get_multipliers() { //[NOTE] Changed this so in the delay when the Link cro
     forward(5,10);
     arm_down();
     forward(-3,10);
+
+}
+void pom_pyramid_multipliers() {
+    msleep(250);
     arm_up();
     rotate(-70);
     
     //msleep(18000); //18 second delay
     //forward(10,30);
-    //[END_CHANGES]
     align_to_line(); // Eliminate error
     forward(3, 0.25);
 
     // Turn and approach tower
-    create3_rotate_degrees(92, 50);
+    create3_rotate_degrees(91, 50);
     create3_wait();
-	slow_servo(arm_grab_position_degrees+50, 1);
-    claw_start();
-    // Execute sections of the total code
-        float velocity = 0.05;
+	slow_servo(arm_grab_position_degrees+37, 1);
+    claw_open();
+    forward(15,0.75);
+    float velocity = 0.015;
     while (velocity != 0) {
         if (create3_sensor_ir(2) >= 900){
         	velocity = 0;
             create3_velocity_set_components(velocity, 0);
-            light_color(255,255,0,1);
+            light_color(255,255,0,0.5);
         }
-        if(analog(5) > 2800) {
+        if(analog(5) > 2080) { //Original value 2750, V2 2550
             velocity = 0;
             create3_velocity_set_components(velocity, 0);
-            light_color(255,0,255,1);
+            light_color(255,0,255,0.5);
         }
         create3_velocity_set_components(velocity, 0);
     }
     create3_wait();
+    create3_velocity_set_components(0.1,0);
+    create3_wait();
+    msleep(1000);
+    create3_velocity_set_components(0,0);
+    create3_wait();
+    rotate(2);
     claw_close();
     msleep(500);
+    forward(-1,0.75);
     arm_up();
+    forward(3,1);
     msleep(500);
-    /*// Return to default position and go to tower
-    slow_servo(arm_grab_position_degrees + 50, 1);
-    set_servo_position(claw_port, 700);
-    forward(10, 0.75);
-    create3_wait();
-	drive_until_bumped();
-    
-     Align to center tower wall
-    float velocity = 0.025;
-    while (velocity != 0) {
-        if (create3_sensor_ir(2) >= 90 || analog(5) > 2700) {
-            velocity = 0;
-        }
-        create3_velocity_set_components(velocity, 0);
-    }
-    create3_wait();*
-    float velocity = 0.08;
-    while (velocity != 0) { // Fancy sensor setup
-        // Better alignment to center tower walls, using "vertical" rangefinder
-        if (analog(5) > 2700) { // Best 2080, V2 2680
-            velocity = 0;
-        }
-        create3_velocity_set_components(velocity, 0);
-    }
-    create3_velocity_set_components(0, 0);
-    //forward(3, 1);
-    //create3_velocity_set_components(0, 0);
-    create3_wait();
-	msleep(500);
-    // Grab cubes
-    claw_close();
-    msleep(750);
-    arm_up();*/
+
     rotate(150);
     arm_down();
     msleep(500);
     claw_open();
     msleep(250);
     arm_up();
-    rotate(-150);
-    forward(0.5, 1);
-    rotate(-90);
-    msleep(1000);
-    forward(24, 1);
+    //rotate(-150);
+    //forward(0.5, 1);
+    //rotate(-90);
+    rotate(30);
+    forward(1.5,30);
     rotate(90);
+    //rotate(120); //Original value 120
+    msleep(1000);
+    forward(18, 0.75);
+    rotate(90);
+    forward(-4,1);
     align_to_line();
-    forward(-1,1);
-    rotate(45);
+    rotate(36); //43.5
     msleep(100);
-    slow_servo(arm_grab_position_degrees + 150, 1); // Second row cubes grab position, original value 125
-    forward(8, 1);
-    velocity = 0.08;
+}
+void orange_pom_area(){
+    slow_servo(arm_grab_position_degrees + 125, 1); // Second row cubes grab position, original value 125
+    forward(4, 1);
+    velocity = 0.025;
     while (velocity != 0) { // Fancy sensor setup
         // Better alignment to center tower walls, using "vertical" rangefinder
-        if (analog(5) > 2780) { // Best 2080, V2 2680
+        if (analog(5) > 2385) { // Best 2080, V2 2680
             velocity = 0;
         }
         create3_velocity_set_components(velocity, 0);
@@ -398,16 +377,19 @@ void get_multipliers() { //[NOTE] Changed this so in the delay when the Link cro
     claw_open();
     msleep(500);
     arm_up();
-    
-	forward(-3,10);
-    rotate(80);
+    rotate(70);
+    forward(10,1);
+    rotate(50); //[V2 30], [V3 50]
+    forward(-3,1);
     align_to_line();
-    rotate(-90);
-    forward(9,30);
+    //rotate(-85);
+    //forward(9,30);
+}
+void purple_tubes(){
     //Purple Noodle Code
     velocity = -0.015;
     while (velocity != 0) { // Code for horizontal alignment, taken from old habitat construction code
-        if ((analog(rangefinder_port) > 1300) || (/*Sensor code*/0)) {
+        if ((analog(rangefinder_port) > 1350)) {
             velocity = 0;
         }
         create3_velocity_set_components(0, velocity);
@@ -416,7 +398,7 @@ void get_multipliers() { //[NOTE] Changed this so in the delay when the Link cro
     //rotate(-2); //Removed because over-compensation for create3 movements
     slow_servo(arm_grab_position_degrees + 5, 1);
     forward(2, 1);
-    while (!(create3_sensor_ir(2) >= 20)) {
+    while (!(create3_sensor_ir(2) >= 30)) { //[V2 40], [V3 3]
         create3_velocity_set_components(0.015, 0);
         create3_wait();
     }
@@ -426,187 +408,9 @@ void get_multipliers() { //[NOTE] Changed this so in the delay when the Link cro
         forward(0.125, 1);
     }
     arm_up();
-    rotate(135);
+    rotate(100);
     drive_until_bumped();
-    /* Removed because this part of the code is obsolete, but is still
-	here because it can be referenced.
-    forward(-15,30);
-    rotate(90);
-    arm_down();
-    msleep(250);
-    claw_open();
-    msleep(500);
-    arm_up();
-    rotate(-135);
-    forward(10,10);
-    align_to_line();*/
-	/*
-    // Not doing rock heap anymore, so first row cubes go to Area 4 (orange poms)
-    msleep(250);
-    forward(0, 1);
-    rotate(-45); //-90
+	rotate(5);
     arm_down();
     msleep(500);
-    claw_open();
-    msleep(500);
-    arm_up();
-    rotate(-45);
-    msleep(1000);
-    forward(24, 1);
-    rotate(90);
-    align_to_line();
-    forward(-1,1);
-    rotate(45);
-    //arm_down();
-    //msleep(500);
-    //claw_open();
-
-    // Grab the second row of cubes
-    msleep(100);
-    slow_servo(arm_grab_position_degrees + 150, 1); // Second row cubes grab position, original value 125
-    forward(8, 1);
-    velocity = 0.08;
-    while (velocity != 0) { // Fancy sensor setup
-        // Better alignment to center tower walls, using "vertical" rangefinder
-        if (analog(5) > 2680) { // Best 2080
-            velocity = 0;
-        }
-        create3_velocity_set_components(velocity, 0);
-    }
-    create3_wait();
-    claw_grab_habitat();
-    msleep(500);
-    arm_up();
-    //forward(-17, 1);
-    align_to_line_backwards();
-
-    // Go to the pom area
-    forward(-6,1);
-    rotate(87);
-    forward(11, 10);
-    align_to_line(); // Moves forward to line to reduce step counting, then turns and drops multipliers*/
-}
-
-void pom_area() {
-    // Execute actions related to handling poms in a designated area
-    // Push poms into the area with multipliers
-    slow_servo(1024, 1);
-    swipe_poms(); // swipe_poms() uses arc turning to push poms into area with multipliers
-    swipe_poms();
-    swipe_poms();
-    rotate(35);
-
-    // Move back and drop cubes
-    forward(0, 1);
-    rotate(15); // Original 10
-    arm_down();
-    msleep(250);
-    claw_open();
-    msleep(250);
-
-    // Set arm position, then move to purple tubes
-    slow_servo(arm_grab_position_degrees + 275, 1);
-    rotate(75); // Original 70
-    forward(30, 1); // Original 20, 2nd original 25
-    rotate(-90); // Original -90, 2nd original 45, 3rd original -80
-    // forward(5,1);
-}
-
-void purple_tubes() {
-    // Execute actions related to handling purple tubes
-    forward(2,10);
-    velocity = -0.015;
-    while (velocity != 0) { // Code for horizontal alignment, taken from old habitat construction code
-        if ((analog(rangefinder_port) > 1300) || (/*Sensor code*/0)) {
-            velocity = 0;
-        }
-        create3_velocity_set_components(0, velocity);
-    }
-    create3_wait();
-    //rotate(-2); //Removed because over-compensation for create3 movements
-    slow_servo(arm_grab_position_degrees + 5, 1);
-    forward(2, 1);
-    while (!(create3_sensor_ir(2) >= 15)) {
-        create3_velocity_set_components(0.015, 0);
-        create3_wait();
-    }
-    claw_grab_habitat();
-    for (int i = 0; i < 5; i += 1) {
-        slow_servo(arm_grab_position_degrees - (i*80), 1);
-        forward(0.125, 1);
-    }
-    msleep(250);
-    //arm_grab();
-    rotate(45);
-    align_to_line();
-    msleep(250);
-    arm_up();
-    drive_until_bumped();
-    arm_grab();
-    forward(0,1);
-    rotate(90);
-    drive_until_bumped();
-    forward(0.875,1);
-    msleep(500);
-    rotate(-27.5);
-    slow_servo(180,1);
-    msleep(500);
-    /*velocity = -0.015; //Removed because the randefinder gets confused
-    while (velocity != 0) { // Code for horizontal alignment, taken from old habitat construction code
-        if ((analog(rangefinder_port) > 2700)) {
-            velocity = 0;
-        }
-        create3_velocity_set_components(0, velocity);
-    }*/
-    create3_wait();
-    msleep(250);
-    drive_until_bumped();
-    msleep(250);
-    velocity = 0.015;
-    while (velocity != 0) { // Code for horizontal alignment, taken from old habitat construction code
-        if ((analog(rangefinder_port) > 2700)) {
-            velocity = 0;
-        }
-        create3_velocity_set_components(0, velocity);
-    }
-    msleep(500);
-    rotate(-2);
-    msleep(500);
-	create3_velocity_set_components(-0.03,0);
-    create3_wait();
-    msleep(300);
-    create3_velocity_set_components(0,0);
-    create3_wait();
-    msleep(500);
-    claw_open();
-    /*msleep(100);
-    while(analog(rangefinder_port)<1750){ //Code for horizontal alignment, taken from old habitat construction code
-        create3_velocity_set_components(0,0.015);
-        create3_wait();
-    }
-    create3_velocity_set_components(0,0);
-    create3_wait();
-    msleep(100);
-    * Removed because the rangefinder is slightly off, so this while loop is just "move forward forever"
-    while(analog(rangefinder_port)<2900){ //repurposed code above, used for precise moving back for claw to be able to grip noodle
-        create3_velocity_set_components(0.005,0);
-        create3_wait();
-    }
-    rotate(6);
-    forward(1.125,1);
-    claw_grab_habitat(); // Tight grip on purple noodle
-    msleep(500);
-    arm_down();
-    msleep(100);
-    rotate(135);
-    forward(6,1);
-    rotate(-45);
-    arm_up();
-    rotate(-45);
-    forward(27,5);
-    rotate(-90);
-    drive_until_bumped();
-    forward(0.5,1);
-    rotate(90);
-    drive_until_bumped();*/
 }
