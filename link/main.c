7aa456d41bcf30772b2df9cc97a84139d73f3fa3
donghol <162074271+donghol@users.noreply.github.com>
2024-07-12 21:33:01 -0700

    jul 12 2024

--- link/main.c
+++ link/main.c
@@ -21,6 +21,8 @@ int right_arm_up=1150; //port # 1
 int right_arm_down=60; //port # 1
 int left_arm_up=780; //port # 0
 int left_arm_down=1911; //port # 0
+int switch_arm_up=2047;//new 
+int switch_arm_down=1600;
 int main() {
     //starting
     enable_servos();
@@ -28,10 +30,10 @@ int main() {
     msleep(500);
     set_servo_position(left_motor_port,left_arm_up);
     msleep(500);
-    set_servo_position(switch_arm,0);
+    set_servo_position(switch_arm,2047);
     msleep(500);
     //exit(1);
-    //wait_for_light();
+    wait_for_light(0);
     msleep(500);
     set_servo_position(0,left_arm_down);
     msleep(500);
@@ -40,7 +42,7 @@ int main() {
 
     //start 1st straight
     set_servo_position(1,1150);
-    move_forward(4600);
+    move_forward(4400);
    
     //turn right
     turn_right(1700);
@@ -50,7 +52,7 @@ int main() {
     move_forward(1700);
     
     //turn left off of bump
-    turn_left(1700);
+    turn_left(1630);
     
     //shimmy
     //shimmy();
@@ -60,13 +62,13 @@ int main() {
     move_forward(2450);
     
     //turn left towards the rock heap
-    turn_left(1500);
-    move_forward(2400);
+    turn_left(1470);
+    move_forward(2800);
     set_servo_position(0,left_arm_up);
     set_servo_position(1,right_arm_up);
     
     //drive into the rock heap
-    move_forward(1000);
+    move_forward(1400);
     
     //curve the rocks into rock heap more securly
     //turn_right(500);
@@ -102,12 +104,22 @@ int main() {
     set_servo_position(1,400);
     motor(0,25);
     motor(1,-75);
-    msleep(1100);
+    msleep(800);
+    ao();
+    msleep(35000);
     //go back from rock heap so that the link is in the middle white area
     motor(1,-100);
     motor(0,-100);
-    msleep(2300);
-    set_servo_position(0,left_arm_down);
+    msleep(400);
+    
+    motor(1,-50);
+    msleep(650);
+    
+    motor(1,-100);
+    motor(0,-100);
+    msleep(1350);
+    set_servo_position(0,left_arm_up);
+    set_servo_position(1,right_arm_up);
     //turns 180 degrees
     
     motor(1,-50);
@@ -115,12 +127,12 @@ int main() {
     msleep(2610);
     //starts going back until the link senses black tape
 	hi();
-    motor(0,100);
-    motor(1,100);
+    motor(0,50);
+    motor(1,50);
     //goes foward so that the front sensor won't be sensing black
     motor(0,50);
     motor(1,50);
-    msleep(7000);
+    msleep(6400);
     ao();
     //start to go back until front sensor senses black
     bye();
@@ -128,36 +140,19 @@ int main() {
     motor(0,-50);
     ao();
     //link will crash to the switch
-   // set_servo_position(0,2047);
-    motor(0,-50);
-    msleep(350);
-    motor(1,-100);
-    motor(0,-100);
-    msleep(2500);
-    //turns 90
-    motor(0,-50);
-    motor(1,50);
-    msleep(1500);
-    set_servo_position(2,0);
-    msleep(200);
-    ao();
-    //forward and then back
-    motor(0,50);
-    motor(1,50);
-    msleep(3000);
-    ao();
-    
-    while(analog(2)<2900){
-    	motor(0,-30);
-        motor(1,-30);
-    }
+   	motor(1,-50);
+   	msleep(100);
+   	motor(1,-70);
+   	motor(0,-70);
+  	msleep(3000);
+    set_servo_position(2, 2046);
     //flip the switch
-    drive_straight(0.5,1000);
-    set_servo_position(2,0);
-    msleep(200);
-    set_servo_position(2,1500);
-    msleep(500);
-    set_servo_position(2,0);
+    //drive_straight(0.5,1000);
+    //set_servo_position(2,0);
+    //msleep(200);
+    //set_servo_position(2,1500);
+    //msleep(500);
+    //set_servo_position(2,0);
     ao();
 }
 
